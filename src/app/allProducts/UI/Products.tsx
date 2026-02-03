c26af96f3b91970b7396596f295839611d74d838
aria_bz <ariabahmanzangi@gmail.com>
2026-02-03 19:43:43 +0330

    finish

--- src/app/allProducts/UI/Products.tsx
+++ src/app/allProducts/UI/Products.tsx
@@ -3,9 +3,14 @@ import { TProductsResponse } from "@/components-main/BestSellers";
 import { useQuery } from "@tanstack/react-query";
 import axios from "@/Api/axios";
 import React from "react";
+import { useSearch } from "@/store/useSearch";
 import Link from "next/link";
+import { useAddToCart } from "@/store/useAddToBasket";
 
 const Products = () => {
+  const { search } = useSearch();
+  const { setProducts, products, clearOne } = useAddToCart();
+
   const { data } = useQuery<TProductsResponse>({
     queryKey: ["products"],
     queryFn: async () => {
@@ -18,107 +23,85 @@ const Products = () => {
       <p className="text-2xl font-medium text-grey700">
         All PROU<span className="border-b-2 border-green-500">UCTS</span>
       </p>
-      <ul className="flex gap-10 items-center flex-wrap ">
-        {data?.products.map((item) => {
-          return (
-            <li
-              key={item._id}
-              className="flex flex-col group gap-2 p-4 border border-gray-300 rounded-lg"
-            >
-              <Link href={`/allProducts/${item.category}/${item._id}`}>
-                <div className="px-5 py-2">
-                  <img
-                    className="max-w-36 group-hover:scale-105"
-                    src={item.image[0]}
-                    alt={item.name}
-                  />
-                </div>
-              </Link>
-              <div className="flex flex-col ">
-                <p className="text-gray-500/60 text-sm">{item.category}</p>
-                <p className="text-grey700 font-medium text-lg ">{item.name}</p>
-                <div className="flex items-center gap-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="17"
-                    viewBox="0 0 18 17"
-                    fill="none"
-                  >
-                    <path
-                      d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                      fill="#4FBF8B"
+      <ul className=" gap-14 items-center grid grid-cols-4 ">
+        {data?.products
+          .filter((product) =>
+            product.name.toLocaleLowerCase().includes(search),
+          )
+          .map((item) => {
+            const inCart = products.find((p) => p._id === item._id);
+            const qty = inCart?.qty || 0;
+            return (
+              <li
+                key={item._id}
+                className="flex flex-col group gap-2 p-6 border border-gray-300 rounded-lg"
+              >
+                <Link href={`/allProducts/${item.category}/${item._id}`}>
+                  <div className="px-5 py-2">
+                    <img
+                      className="max-w-full group-hover:scale-105"
+                      src={item.image[0]}
+                      alt={item.name}
                     />
-                  </svg>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="17"
-                    viewBox="0 0 18 17"
-                    fill="none"
-                  >
-                    <path
-                      d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                      fill="#4FBF8B"
-                    />
-                  </svg>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="17"
-                    viewBox="0 0 18 17"
-                    fill="none"
-                  >
-                    <path
-                      d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                      fill="#4FBF8B"
-                    />
-                  </svg>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="17"
-                    viewBox="0 0 18 17"
-                    fill="none"
-                  >
-                    <path
-                      d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                      fill="#4FBF8B"
-                    />
-                  </svg>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="17"
-                    viewBox="0 0 18 17"
-                    fill="none"
-                  >
-                    <path
-                      d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                      fill="#4FBF8B"
-                    />
-                  </svg>
-                  <p className="text-sm text-gray-500/60">(4)</p>
-                </div>
-                <div className="flex justify-between pt-4">
-                  <div className="flex gap-1 items-center">
-                    <p className="text-green-500 font-bold text-xl">
-                      ${item.offerPrice}
-                    </p>
-                    <p className="text-sm self-end text-gray-500/60 ">
-                      <span className="line-through">${item.price}</span>
-                    </p>
                   </div>
-                  <div className="flex items-center cursor-pointer justify-center gap-1 bg-[#e1fee2] border border-[#4fbf8b] px-4 py-1 rounded-sm">
-                    <img src="./buy.svg" alt="" />
+                </Link>
+                <div className="flex flex-col ">
+                  <p className="text-gray-500/60 text-sm">{item.category}</p>
+                  <p className="text-grey700 font-medium text-lg ">
+                    {item.name}
+                  </p>
+                  <div className="flex items-center gap-1">
+                    <img className="w-3.5" src="./starpour.svg" alt="" />
 
-                    <button className="cursor-pointer">Add</button>
+                    <img className="w-3.5" src="./starpour.svg" alt="" />
+
+                    <img className="w-3.5" src="./starpour.svg" alt="" />
+
+                    <img className="w-3.5" src="./starpour.svg" alt="" />
+
+                    <img className="w-3.5" src="./stargray.svg" alt="" />
+
+                    <p className="text-sm text-gray-500/60">(4)</p>
+                  </div>
+                  <div className="flex justify-between pt-4">
+                    <div className="flex gap-1 items-center">
+                      <p className="text-primary font-bold text-xl">
+                        ${item.price}
+                      </p>
+                      <p className="text-sm self-end text-gray-500/60 ">
+                        <span className="line-through">${item.offerPrice}</span>
+                      </p>
+                    </div>
+                    {qty === 0 ? (
+                      <div className="flex items-center cursor-pointer justify-center gap-1 bg-[#e1fee2] border border-[#4fbf8b] px-4 py-1 rounded-sm">
+                        <img src="./buy.svg" alt="" />
+
+                        <button onClick={() => setProducts(item)}>Add</button>
+                      </div>
+                    ) : (
+                      <div className="flex items-center cursor-pointer justify-center gap-2 bg-[#e1fee2] border border-[#4fbf8b] px-4 py-1 rounded-sm">
+                        <p
+                          onClick={() => {
+                            clearOne(item._id);
+                          }}
+                        >
+                          -
+                        </p>
+                        <p>{qty}</p>
+                        <p
+                          onClick={() => {
+                            setProducts(item);
+                          }}
+                        >
+                          +
+                        </p>
+                      </div>
+                    )}
                   </div>
                 </div>
-              </div>
-            </li>
-          );
-        })}
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
--- src/app/allProducts/[...id]/EachInformation.tsx
+++ src/app/allProducts/[...id]/EachInformation.tsx
@@ -5,6 +5,7 @@ import axios from "@/Api/axios";
 import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useAddToCart } from "@/store/useAddToBasket";
 
 const EachInformation = ({ postId }: { postId: {}[] }) => {
   const route = useRouter();
@@ -16,6 +17,8 @@ const EachInformation = ({ postId }: { postId: {}[] }) => {
       return data;
     },
   });
+
+  const { setProducts, products, clearOne } = useAddToCart();
 
   const filter = data?.products.filter((each) => each._id === postId[1]);
   const category = data?.products.filter((c) => c.category === postId[0]);
@@ -31,7 +34,7 @@ const EachInformation = ({ postId }: { postId: {}[] }) => {
                 {one.category} /
               </Link>
               <Link
-                className="text-green-500"
+                className="text-primary"
                 href={`/allProducts/${one.category}/${one._id}`}
               >
                 {one.name}
@@ -60,71 +63,11 @@ const EachInformation = ({ postId }: { postId: {}[] }) => {
                 <div className="flex flex-col gap-2">
                   <p className="text-3xl font-medium">{one.name}</p>
                   <div className="flex items-center gap-1">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="17"
-                      viewBox="0 0 18 17"
-                      fill="none"
-                    >
-                      <path
-                        d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                        fill="#4FBF8B"
-                        fillOpacity="0.35"
-                      />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="17"
-                      viewBox="0 0 18 17"
-                      fill="none"
-                    >
-                      <path
-                        d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                        fill="#4FBF8B"
-                        fillOpacity="0.35"
-                      />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="17"
-                      viewBox="0 0 18 17"
-                      fill="none"
-                    >
-                      <path
-                        d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                        fill="#4FBF8B"
-                        fillOpacity="0.35"
-                      />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="17"
-                      viewBox="0 0 18 17"
-                      fill="none"
-                    >
-                      <path
-                        d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                        fill="#4FBF8B"
-                        fillOpacity="0.35"
-                      />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="17"
-                      viewBox="0 0 18 17"
-                      fill="none"
-                    >
-                      <path
-                        d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                        fill="#4FBF8B"
-                        fillOpacity="0.35"
-                      />
-                    </svg>
+                    <img className="w-3.5" src="/starpour.svg" alt="" />
+                    <img className="w-3.5" src="/starpour.svg" alt="" />
+                    <img className="w-3.5" src="/starpour.svg" alt="" />
+                    <img className="w-3.5" src="/starpour.svg" alt="" />
+                    <img className="w-3.5" src="/stargray.svg" alt="" />
                     <p className="text-sm text-gray-500/60">(4)</p>
                   </div>
                 </div>
@@ -151,10 +94,21 @@ const EachInformation = ({ postId }: { postId: {}[] }) => {
                   ))}
                 </ul>
                 <div className="flex items-center  gap-4 text-base w-full">
-                  <button className="w-full py-3.5 cursor-pointer font-medium bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition-all">
+                  <button
+                    onClick={() => {
+                      setProducts(one);
+                    }}
+                    className="w-full py-3.5 cursor-pointer font-medium bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition-all"
+                  >
                     Add to Cart
                   </button>
-                  <button className="w-full py-3.5 cursor-pointer font-medium bg-primary text-white bg-green-600 transition-all">
+                  <button
+                    onClick={() => {
+                      setProducts(one);
+                      route.push("/cart");
+                    }}
+                    className="w-full py-3.5 cursor-pointer font-medium bg-green-600 text-white transition-all"
+                  >
                     Buy now
                   </button>
                 </div>
@@ -169,6 +123,9 @@ const EachInformation = ({ postId }: { postId: {}[] }) => {
         </p>
         <ul className="flex gap-10 items-center flex-wrap">
           {category?.map((item) => {
+            const inCart = products.find((p) => p._id === item._id);
+            const qty = inCart?.qty || 0;
+
             return (
               <li
                 key={item._id}
@@ -189,71 +146,16 @@ const EachInformation = ({ postId }: { postId: {}[] }) => {
                     {item.name}
                   </p>
                   <div className="flex items-center gap-1">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="17"
-                      viewBox="0 0 18 17"
-                      fill="none"
-                    >
-                      <path
-                        d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                        fill="#4FBF8B"
-                        fillOpacity="0.35"
-                      />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="17"
-                      viewBox="0 0 18 17"
-                      fill="none"
-                    >
-                      <path
-                        d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                        fill="#4FBF8B"
-                        fillOpacity="0.35"
-                      />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="17"
-                      viewBox="0 0 18 17"
-                      fill="none"
-                    >
-                      <path
-                        d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                        fill="#4FBF8B"
-                        fillOpacity="0.35"
-                      />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="17"
-                      viewBox="0 0 18 17"
-                      fill="none"
-                    >
-                      <path
-                        d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                        fill="#4FBF8B"
-                        fillOpacity="0.35"
-                      />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="17"
-                      viewBox="0 0 18 17"
-                      fill="none"
-                    >
-                      <path
-                        d="M8.04894 0.927049C8.3483 0.00573802 9.6517 0.00574017 9.95106 0.927051L11.2451 4.90983C11.379 5.32185 11.763 5.60081 12.1962 5.60081H16.3839C17.3527 5.60081 17.7554 6.84043 16.9717 7.40983L13.5838 9.87132C13.2333 10.126 13.0866 10.5773 13.2205 10.9894L14.5146 14.9721C14.8139 15.8934 13.7595 16.6596 12.9757 16.0902L9.58778 13.6287C9.2373 13.374 8.7627 13.374 8.41221 13.6287L5.02426 16.0902C4.24054 16.6596 3.18607 15.8934 3.48542 14.9721L4.7795 10.9894C4.91338 10.5773 4.76672 10.126 4.41623 9.87132L1.02827 7.40983C0.244561 6.84043 0.647338 5.60081 1.61606 5.60081H5.8038C6.23703 5.60081 6.62099 5.32185 6.75486 4.90983L8.04894 0.927049Z"
-                        fill="#4FBF8B"
-                        fillOpacity="0.35"
-                      />
-                    </svg>
+                    <img className="w-3.5" src="./starpour.svg" alt="" />
+
+                    <img className="w-3.5" src="./starpour.svg" alt="" />
+
+                    <img className="w-3.5" src="./starpour.svg" alt="" />
+
+                    <img className="w-3.5" src="./starpour.svg" alt="" />
+
+                    <img className="w-3.5" src="./stargray.svg" alt="" />
+
                     <p className="text-sm text-gray-500/60">(4)</p>
                   </div>
                   <div className="flex justify-between pt-4">
@@ -265,11 +167,31 @@ const EachInformation = ({ postId }: { postId: {}[] }) => {
                         <span className="line-through">${item.offerPrice}</span>
                       </p>
                     </div>
-                    <div className="flex items-center cursor-pointer justify-center gap-1 bg-[#e1fee2] border border-[#4fbf8b] px-4 py-1 rounded-sm">
-                      <img src="./buy.svg" alt="" />
+                    {qty === 0 ? (
+                      <div className="flex items-center cursor-pointer justify-center gap-1 bg-[#e1fee2] border border-[#4fbf8b] px-4 py-1 rounded-sm">
+                        <img src="./buy.svg" alt="" />
 
-                      <button className="cursor-pointer">Add</button>
-                    </div>
+                        <button onClick={() => setProducts(item)}>Add</button>
+                      </div>
+                    ) : (
+                      <div className="flex items-center cursor-pointer justify-center gap-2 bg-[#e1fee2] border border-[#4fbf8b] px-4 py-1 rounded-sm">
+                        <p
+                          onClick={() => {
+                            clearOne(item._id);
+                          }}
+                        >
+                          -
+                        </p>
+                        <p>{qty}</p>
+                        <p
+                          onClick={() => {
+                            setProducts(item);
+                          }}
+                        >
+                          +
+                        </p>
+                      </div>
+                    )}
                   </div>
                 </div>
               </li>
